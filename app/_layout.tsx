8855e1dd7d37a8ab9e726863069480bbebed1fbd
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2025-03-20 17:32:33 -0300

    updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,18 @@
-import database from "@/db";
+// import database from "@/db";
+import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
 import "react-native-reanimated";
 
 export default function RootLayout() {
-  const postsCollection = database.get("posts");
-  console.log(postsCollection);
+  // const postsCollection = database.get("posts");
+  // console.log(postsCollection);
 
   return (
-    <View
-      style={{
-        backgroundColor: "#000",
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ color: "#fff" }}>Hello world!</Text>
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
       <StatusBar style="light" />
-    </View>
+    </>
   );
 }
