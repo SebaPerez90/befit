b1801380a65de8e7e06697acb4a13ec507f699b3
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2025-03-21 17:53:56 -0300

    updates

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,66 +1,15 @@
-import { View, Text, Pressable } from "react-native";
-import React from "react";
-import * as SQLite from "expo-sqlite";
-import { drizzle } from "drizzle-orm/expo-sqlite";
-// import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
-// import migrations from "../drizzle/migrations";
-import { eq } from "drizzle-orm";
-import { postsTable } from "../db/schema/post";
-import { usersTable } from "../db/schema/users";
-
-const expo = SQLite.openDatabaseSync("db.db");
-const db = drizzle(expo);
+import React from 'react';
+import { CustomText } from '../modules/core/components/CustomText';
+import { ScreenView } from '../modules/core/components/ScreenView';
 
 export default function index() {
-  // const { success, error } = useMigrations(db, migrations);
-
-  const getUsers = async () => {
-    const users = await db.select().from(usersTable);
-    const posts = await db.select().from(postsTable);
-    console.log("users: ", users);
-    console.log("post: ", posts);
-  };
-
-  const deleteUsers = async () => {
-    try {
-      const users = await db
-        .delete(usersTable)
-        .where(eq(usersTable.name, "Sebastian"));
-      return { message: "usuario eliminado", userData: users };
-    } catch (error) {
-      console.error("Error deleting users: ", error);
-    }
-  };
-
-  const createUser = async () => {
-    return db.insert(usersTable).values({
-      name: "Sebastian",
-      age: 30,
-      email: "john@example.com",
-    });
-  };
-
   return (
-    <View className="flex-1 bg-black justify-center items-center">
-      <Text className="text-2xl text-white">Hello world!</Text>
-      <Pressable
-        className="rounded-lg bg-blue-500 py-3 px-5 mt-8"
-        onPress={getUsers}
-      >
-        <Text className="text-white">View users</Text>
-      </Pressable>
-      <Pressable
-        className="rounded-lg bg-red-500 py-3 px-5 mt-8"
-        onPress={deleteUsers}
-      >
-        <Text className="text-white">Delete users</Text>
-      </Pressable>
-      <Pressable
-        className="rounded-lg bg-green-500 py-3 px-5 mt-8"
-        onPress={createUser}
-      >
-        <Text className="text-white">Create users</Text>
-      </Pressable>
-    </View>
+    <ScreenView>
+      <CustomText type='title'>Hello World!</CustomText>
+      <CustomText type='subtitle'>Hello World!</CustomText>
+      <CustomText type='defaultSemiBold'>Hello World!</CustomText>
+      <CustomText>Hello World!</CustomText>
+      <CustomText type='link'>Hello World!</CustomText>
+    </ScreenView>
   );
 }
---
+++ src/modules/user/services/index.ts
@@ -0,0 +1,42 @@
+import * as SQLite from "expo-sqlite";
+
+import { drizzle } from "drizzle-orm/expo-sqlite";
+// import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+// import migrations from "../drizzle/migrations";
+import { eq } from "drizzle-orm";
+
+import { postsTable } from "@/src/db/schema/post";
+import { usersTable } from "@/src/db/schema/users";
+
+const expo = SQLite.openDatabaseSync("db.db");
+const db = drizzle(expo);
+
+// const { success, error } = useMigrations(db, migrations);
+
+export const getUsers = async () => {
+  const users = await db.select().from(usersTable);
+  const posts = await db.select().from(postsTable);
+  console.log("users: ", users);
+  console.log("post: ", posts);
+};
+
+export const deleteUsers = async () => {
+  try {
+    const users = await db
+      .delete(usersTable)
+      .where(eq(usersTable.name, "Sebastian"));
+    return { message: "usuario eliminado", userData: users };
+  } catch (error) {
+    console.error("Error deleting users: ", error);
+  }
+};
+
+export const createUser = async () => {
+  const response = await db.insert(usersTable).values({
+    name: "Sebastian",
+    age: 30,
+    email: "seeebas22@example.com",
+  });
+
+  console.log(response);
+};
